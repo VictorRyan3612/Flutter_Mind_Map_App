f19779b58e6b54165d7cab7ffaf3df7313efade0
Victor Ryan <vitorsilva3612@gmail.com>
2024-09-24 18:01:17 -0300

    feat: graph with nodes

--- mind_map_app/lib/screens/graph_screen.dart
+++ mind_map_app/lib/screens/graph_screen.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:mind_map_app/data/nodes_data_service.dart';
+import 'package:mind_map_app/widgets/edges_painter.dart';
+import 'package:mind_map_app/widgets/node_widget.dart';
 
 class GraphScreen extends StatelessWidget {
   const GraphScreen({super.key});
@@ -12,18 +14,70 @@ class GraphScreen extends StatelessWidget {
     Timer? _debounce;
 
     return InteractiveViewer(
-      child: GestureDetector(
-        onDoubleTapDown: (details) {
+      child: ValueListenableBuilder(
+        valueListenable: nodesDataService.nodes,
+        builder: (context, nodesValue, child) {
           
-          _debounce = Timer(const Duration(milliseconds: 500), () {
-            if (nodesDataService.firstSelectedNode.value != null) {
+          return GestureDetector(
+            onTap: () {
+              nodesDataService.firstSelectedNode.value = null;
+
+            },
+            onDoubleTapDown: (details) {
+              _debounce = Timer(const Duration(milliseconds: 500), () {
+                if (nodesDataService.firstSelectedNode.value == null) {
+
+
+                  showContextMenu(context, positionOffset: details.localPosition);
+                }
+              });
+            },
+            onSecondaryTapDown: (details) {
+              nodesDataService.firstSelectedNode.value = null;
+              nodesValue.add(Node(color: Colors.red));
+              nodesDataService.nodes.notifyListeners();
               showContextMenu(context, positionOffset: details.localPosition);
-            }
-          });
-        },
-        onSecondaryTapDown: (details) {
-          showContextMenu(context, positionOffset: details.localPosition);
-        },
+            },
+            child: Stack(
+              children: [
+                ValueListenableBuilder(
+                  valueListenable: nodesDataService.edges,
+                  builder: (context, edgesValue, child) {
+                    return CustomPaint(
+                      size: Size.infinite,
+                      painter: EdgesPainter([]),
+                    );
+                  },
+                ),
+                for (int i = 0; i < nodesValue.length; i++)
+                Positioned(
+                  left: nodesValue[i].position.dx,
+                  top: nodesValue[i].position.dy,
+                  child: GestureDetector(
+                    onDoubleTapDown: (details) {
+                      nodesDataService.isEditing.value = true;
+                      nodesDataService.firstSelectedNode.value = nodesValue[i];
+                    },
+                    onTapDown: (details) {
+                      nodesDataService.firstSelectedNode.value = nodesValue[i];
+                      
+                    },
+                    onSecondaryTapDown: (details) {
+                      nodesDataService.firstSelectedNode.value = nodesValue[i];
+                    },
+                    onPanUpdate: (details) {
+                      nodesValue[i].position += details.delta;
+                      nodesDataService.nodes.notifyListeners();
+                    },
+                    child: NodeWidget(
+                      node: nodesValue[i],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        }
       ),
     );
   }
