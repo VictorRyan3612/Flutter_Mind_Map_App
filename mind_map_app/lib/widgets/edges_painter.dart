27e69f7778b8677ee561a1b09330a49eed1323d9
Victor Ryan <vitorsilva3612@gmail.com>
2024-09-24 22:42:45 -0300

    feat: edgesPainter part 2

--- mind_map_app/lib/widgets/edges_painter.dart
+++ mind_map_app/lib/widgets/edges_painter.dart
@@ -24,11 +24,38 @@ class EdgesPainter extends CustomPainter {
         ..color = edge.color
         ..strokeWidth = edge.size
         ..style = PaintingStyle.stroke;
-        
+
       double angle;
+      if (edge.curvad) {
+        final controlPoint1 = getControlPoint(startBorder, endBorder, -20); // Primeiro ponto de controle (acima)
+        final controlPoint2 = getControlPoint(startBorder, endBorder, 20);  // Segundo ponto de controle (abaixo)
+
+        final path = Path()
+          ..moveTo(startBorder.dx, startBorder.dy)
+          ..cubicTo(controlPoint1.dx, controlPoint1.dy, controlPoint2.dx, controlPoint2.dy, endBorder.dx, endBorder.dy);
+
+        canvas.drawPath(path, paint);
+        
+        // Calcular o ângulo da seta no final
+        angle = atan2(endBorder.dy - startBorder.dy, endBorder.dx - startBorder.dx);
+      }
+      else {
+        // Desenha linha reta entre os pontos de borda dos nós
+        canvas.drawLine(startBorder, endBorder, paint);
+
+        // Calcular o ângulo da seta na linha reta
+        angle = atan2(endBorder.dy - startBorder.dy, endBorder.dx - startBorder.dx);
+      }
     }
   }
-  
+
+  Offset getControlPoint(Offset start, Offset end, double offset) {
+    return Offset(
+      (start.dx + end.dx) / 2,
+      (start.dy + end.dy) / 2 + offset, // Ajusta a altura do ponto de controle para criar a ondulação
+    );
+  }
+
   Offset getBorderPosition(Node from, Node to) {
     final center = Offset(from.position.dx + from.width / 2, from.position.dy + from.height / 2);
     final direction = (to.position - from.position).direction;
