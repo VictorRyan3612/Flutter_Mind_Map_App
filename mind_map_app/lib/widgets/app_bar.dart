9af34aecd1786d4af490a792b1abe12605af3738
Victor Ryan <vitorsilva3612@gmail.com>
2024-12-17 06:29:05 -0300

    feat: modeMindMap in app_bar

--- mind_map_app/lib/widgets/app_bar.dart
+++ mind_map_app/lib/widgets/app_bar.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:mind_map_app/data/nodes_data_service.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget{
   String title;
-  MyAppBar({super.key, this.title =''});
+  bool modeMindMap;
+  MyAppBar({super.key, this.title ='', required this.modeMindMap});
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> listIcons = [];
+    if (modeMindMap) {
+      
+    } else {
+      listIcons.add(
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {
+            if(nodesDataService.mindMap.value != null){
+              nodesDataService.saveMindMap(nodesDataService.mindMap.value!);
+            }
+          }
+        ),
+      );
+    }
     return AppBar(
       title: Text(title),
       actions: [
+        ...listIcons,
         IconButton(
           icon: Icon(Icons.settings),
           onPressed: () => Navigator.pushNamed(context, '/configs'),
