1b42f6b0ba3e346a7e38125cc116ceb62593b734
Victor Ryan <vitorsilva3612@gmail.com>
2024-12-17 06:40:46 -0300

    refact: removing MainApp not necessary

--- mind_map_app/lib/main.dart
+++ mind_map_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_vicr_widgets/flutter_vicr_widgets.dart';
 import 'package:mind_map_app/data/nodes_data_service.dart';
 import 'package:mind_map_app/screens/graph_screen.dart';
+import 'package:mind_map_app/screens/mindmap_list_screen.dart';
 
 void main() {
   
@@ -13,35 +14,12 @@ void main() {
       materialApp: MaterialApp(
         debugShowCheckedModeBanner: false,
         routes: {
-          '/': (context) => MainApp(),
+          '/': (context) => MindMapListScreen(),
+          '/map': (context) => GraphScreen()
         }
       ),
     )
   );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(actions: [
-        IconButton(
-          onPressed: (){
-            if(nodesDataService.mindMap.value != null){
-              nodesDataService.saveMindMap(nodesDataService.mindMap.value!);
-            }
-          }, 
-          icon: Icon(Icons.add)
-        ),
-        IconButton(
-          icon: Icon(Icons.settings),
-          onPressed: () => Navigator.pushNamed(context, '/configs'),
-        ),
-      ],),
-      body: GraphScreen()
-    );
-  }
-}
