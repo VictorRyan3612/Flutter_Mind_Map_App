800efcfc021172820ae72cc6e6a9a68f80ab6bb1
Victor Ryan <vitorsilva3612@gmail.com>
2025-01-20 21:37:20 -0300

    refact: node_widget remove updateSize add Intrinsic

--- mind_map_app/lib/widgets/node_widget.dart
+++ mind_map_app/lib/widgets/node_widget.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -24,85 +25,67 @@ class NodeWidget extends HookWidget {
     final textColor = getContrastingTextColor(node.color);
     final textController = useTextEditingController(text: node.text);
     final focusNode = useFocusNode();
-    final sizeNotifier = useState<Size>(Size(node.width, node.height));
     final isInitialized = useState<bool>(false);
     Timer? _debounce;
 
-    void updateSize() {
-      final textLength = textController.text.length;
-final baseHeight = 50.0;
-      // Calculando a largura e a altura do texto
-      final textPainter = TextPainter(
-        text: TextSpan(
-          text: textController.text,
-          style: TextStyle(fontSize: 16.0), // Ajuste o tamanho da fonte conforme necessário
-        ),
-        maxLines: null, // Para permitir múltiplas linhas
-        textDirection: TextDirection.ltr,
-      );
-
-      textPainter.layout(minWidth: 0, maxWidth: double.infinity);
-
-      double newWidth = min(max(75.0, textPainter.width + 50), 400.0); // Adiciona algum padding
-      double newHeight;
-      // if(textController.text.endsWith('\n')){
-      //   newHeight = min(max(baseHeight + (textLength > 30 ? ((textLength - 30) ~/ 10) * 10 : 0), textPainter.height + 20), 300.0); 
-      // }
-      // else{
-        newHeight = baseHeight + (textLength > 30 ? ((textLength - 30) ~/ 10) * 10 : 0); 
-      // }
-
-      // Atualiza o tamanho se necessário
-      if (newWidth != sizeNotifier.value.width || newHeight != sizeNotifier.value.height) {
-        sizeNotifier.value = Size(newWidth, newHeight);
-        node.width = newWidth;
-        node.height = newHeight;
-      }
-    }
-
     useEffect(() {
       if (!isInitialized.value) {
-        updateSize();
         isInitialized.value = true;
       }
-      textController.addListener(updateSize);
-      return () => textController.removeListener(updateSize);
+      textController.addListener(() {});
+      return () => textController.removeListener(() {});
     }, [textController]);
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        width: sizeNotifier.value.width,
-        height: sizeNotifier.value.height,
-        decoration: BoxDecoration(
-          color: node.color,
-          borderRadius: node.borderRadius,
-        ),
-        child: ValueListenableBuilder(
-          valueListenable: nodesDataService.isEditing,
-          builder: (context, isEditingValue, child) {
-            return Center(
-              child: TextField(
-                onChanged: (value) {
-                  _debounce?.cancel();
-                  _debounce = Timer(const Duration(milliseconds: 500), () {
-                    node.text = value;
-                  });
-                },
-                textAlign: TextAlign.center,
-                controller: textController,
-                maxLines: null,
-                enabled: isEditingValue && nodesDataService.firstSelectedNode.value == node, // Verifica se é o nó em edição,
-                focusNode: focusNode,
-                style: TextStyle(color: textColor), // Define a cor do texto dinamicamente
-
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintStyle: TextStyle(color: Colors.grey),
+      child: IntrinsicWidth(
+        child: IntrinsicHeight(
+          child: Container(
+            constraints: const BoxConstraints(
+              minWidth: 50.0,
+              minHeight: 50,
+              maxWidth: 400.0,
+              maxHeight: 400,
+            ),
+            decoration: BoxDecoration(
+              color: node.color,
+              borderRadius: node.borderRadius,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (node.image != null && node.image!.isNotEmpty)
+                  Image.file(
+                    File(node.image!),
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                Flexible(
+                  child: TextField(
+                    onChanged: (value) {
+                      _debounce?.cancel();
+                      _debounce =
+                          Timer(const Duration(milliseconds: 500), () {
+                        node.text = value;
+                      });
+                    },
+                    textAlign: TextAlign.center,
+                    controller: textController,
+                    maxLines: null,
+                    enabled: nodesDataService.isEditing.value &&
+                        nodesDataService.firstSelectedNode.value == node,
+                    focusNode: focusNode,
+                    style: TextStyle(color: textColor),
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintStyle: TextStyle(color: Colors.grey),
+                    ),
+                  ),
                 ),
-              ),
-            );
-          },
+              ],
+            ),
+          ),
         ),
       ),
     );
